911ce9639a0a5c002558ad62666136e5d1a65e35
Michael <m7qian@uwaterloo.ca>
2023-11-23 05:37:19 -0500

    fuk u sterling

--- client/js/classes/Player.js
+++ client/js/classes/Player.js
@@ -28,15 +28,21 @@ class Player{
         let currentLines = levelZero.lines
         this.checkLineCollisions(currentLines)
 
+        
         this.applyPlayerMovement()
         this.applyGravity()
+
+        // Simple ground collision (change later to lines)
+        // if (this.y + this.height > canvas.height) {
+        //     this.y = canvas.height - this.height;
+        // }
     }
 
     applyPlayerMovement(){
-        if(this.rightHeld && this.onPlatform){
+        if(this.rightHeld){
             this.x += this.playerMovementSpeed
         }
-        else if(this.leftHeld && this.onPlatform){
+        else if(this.leftHeld){
             this.x += -this.playerMovementSpeed
         }
         else{
@@ -45,11 +51,18 @@ class Player{
     }
 
     applyGravity(){
-      
+        
+        
         if(!this.onPlatform){
             this.y += this.velocity.y;
             this.velocity.y = Math.min(this.velocity.y + this.gravity, this.maxVerticalSpeed)
         }
+        else{
+            this.velocity.y = 0
+        }
+      
+
+     
         // console.log(this.velocity.y)
     }
 
@@ -59,26 +72,15 @@ class Player{
         for(let i = 0; i < currentLines.length; i++){
             if(this.isCollidingWithLine(currentLines[i])) collidedLines.push(currentLines[i])
         }
-
-        // ????
-        if(collidedLines.length == 0) this.onPlatform = false;    
-        for(let i = 0; i < collidedLines.length; i++){
-            if(collidedLines[i].isHorizontal){
-                if( this.y < collidedLines[i].y1 && collidedLines[i].y1 < this.y + this.height){
-                    this.onPlatform = true
-                    this.y = collidedLines[i].y1 - this.height
-                }
-                else{
-                    this.onPlatform = false
-                    this.velocity.y = 0
-                    this.velocity.x = 0
-                }
-            }
-            else if(collidedLines[i].isVertical){
-                this.onPlatform = true
-                this.y = collidedLines[i].y1 - this.height
-            }
-        }     
+        if(collidedLines.length == 0){
+            this.onPlatform = false
+        }
+        console.log(collidedLines.length +"," + this.onPlatform +"," + this.velocity.y )
+       
+        // if(collidedLines.length != 0)console.log(collidedLines)
+        // for(let i = 0; i < collidedLines.length; i++){
+        //     if(collidedLines[i].isVertical) console.log(collidedLines[i])
+        // }
     }
 
     isCollidingWithLine(line){
@@ -86,25 +88,55 @@ class Player{
         var isPlayerWithinLineX
         var isPlayerWithinLineY 
         if (line.isHorizontal) {
-            isPlayerWithinLineX = (line.x1 < this.x && this.x < line.x2) || (line.x1 < this.x + this.width && this.x + this.width < line.x2) || (this.x < line.x1 && line.x1 < this.x + this.width) || (this.x < line.x2 && line.x2 < this.x + this.width);
+            isPlayerWithinLineX = (line.x1 < this.x && this.x < line.x2) || (line.x1 < this.x + this.width && this.x + this.width < line.x2) 
+            || (this.x < line.x1 && line.x1 < this.x + this.width) || (this.x < line.x2 && line.x2 < this.x + this.width);
             isPlayerWithinLineY = this.y < line.y1 && line.y1 < this.y + this.height;
 
+            if(isPlayerWithinLineX && isPlayerWithinLineY && !this.onPlatform){
+                  
+                    this.onPlatform = true
+                  
+                    this.y = line.y1 - this.height
+                
+             
+                // console.log("hor")
+               
+            }
+          
             return isPlayerWithinLineX && isPlayerWithinLineY;
 
         }
         else if (line.isVertical) {
             
-            isPlayerWithinLineY = (line.y1 < this.y && this.y < line.y2) || (line.y1 < this.y + this.height && this.y + this.height < line.y2) || (this.y < line.y1 && line.y1 < this.y + this.height) || (this.y < line.y2 && line.y2 < this.y + this.height);
+            isPlayerWithinLineY = (line.y1 < this.y && this.y < line.y2) || (line.y1 < this.y + this.height && this.y + this.height < line.y2) 
+            || (this.y < line.y1 && line.y1 < this.y + this.height) || (this.y < line.y2 && line.y2 < this.y + this.height);
+            
             isPlayerWithinLineX = this.x < line.x1 && line.x1 < this.x + this.width;
+
+            
+  
+            if(isPlayerWithinLineX && isPlayerWithinLineY ) {
+                if( this.y < line.y1 && line.y1 < this.y + this.height  && !this.onPlatform){
+                    this.onPlatform = true
+                    this.y = line.y1 - this.height
+                
+                    console.log("dab")
+                }
+             
+              
+            }
 
             return isPlayerWithinLineX && isPlayerWithinLineY;
         }
 
+       
+        console.log("Dab"+this.onPlatform)
         return false
     }
 
     jump(){
         this.velocity.y = this.jumpStrength;
+        this.y += this.velocity.y
         this.onPlatform = false;
     }
 }
