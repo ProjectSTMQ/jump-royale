38406ba30d78e6fc2fa7f9e6ca143f834f3407ce
sterling-tenn <sterling8302@gmail.com>
2023-11-24 17:12:36 -0500

    Update Player.js

--- client/js/classes/Player.js
+++ client/js/classes/Player.js
@@ -30,39 +30,29 @@ class Player{
     update(){
         this.draw()
         let currentLines = levelZero.lines
-        this.checkLineCollisions(currentLines)
-
-        
+        this.checkLineCollisions(currentLines)  
         this.applyPlayerMovement()
         this.updateJumpStrength()        
         this.applyGravity()
-
-        // Simple ground collision (change later to lines)
-        // if (this.y + this.height > canvas.height) {
-        //     this.y = canvas.height - this.height;
-        // }
     }
 
     applyPlayerMovement(){
-        if(this.rightHeld){
+        if(this.rightHeld && this.onPlatform){
             this.velocity.x = this.playerMovementSpeed
         }
-        else if(this.leftHeld){
+        else if(this.leftHeld && this.onPlatform){
             this.velocity.x = -this.playerMovementSpeed
         }
         else if(this.onPlatform){
-            this.velocity.x =0;
+            this.velocity.x = 0;
         }
 
         if(!this.jumpHeld){
             this.x += this.velocity.x;
         }
-       
     }
 
     applyGravity(){
-        
-        
         if(!this.onPlatform){
             this.y += this.velocity.y;
             this.velocity.y = Math.min(this.velocity.y + this.gravity, this.maxVerticalSpeed)
@@ -70,10 +60,6 @@ class Player{
         else{
             this.velocity.y = 0
         }
-      
-
-     
-        // console.log(this.velocity.y)
     }
 
 
@@ -88,86 +74,57 @@ class Player{
         if(collidedLines.length == 0){
             this.onPlatform = false
         }
-       // console.log(collidedLines.length +"," + this.onPlatform +"," + this.velocity.y )
-       
-        // if(collidedLines.length != 0)console.log(collidedLines)
-        // for(let i = 0; i < collidedLines.length; i++){
-        //     if(collidedLines[i].isVertical) console.log(collidedLines[i])
-        // }
     }
 
     handleCollision(line){
-
         if(line.isHorizontal){
             console.log("horiz")
             if((this.velocity.y > 0) && !this.onPlatform){
-                
                 this.onPlatform = true
-        
                 this.y = line.y1 - this.height
-
             }
             else{
-                
                 this.velocity.y = -this.velocity.y /3
                 this.y = line.y1        
                 this.y += this.velocity.y
             }
         }
         else if(line.isVertical){
-          
-
             if(this.velocity.x > 0){
                 console.log("fuck2")
                 this.x = line.x1 - this.width
-           
             }
             else{
                 this.x = line.x1
             }
-
-
             this.velocity.x = -this.velocity.x
-            
         }
     }
 
     isCollidingWithLine(line){
-      
         var isPlayerWithinLineX
         var isPlayerWithinLineY 
 
         if (line.isHorizontal) {
-            
             isPlayerWithinLineX = (line.x1 < this.x && this.x < line.x2) || (line.x1 < this.x + this.width && this.x + this.width < line.x2) 
             || (this.x < line.x1 && line.x1 < this.x + this.width) || (this.x < line.x2 && line.x2 < this.x + this.width);
             
             isPlayerWithinLineY = this.y < line.y1 && line.y1 < this.y + this.height;
-
-           
-
         }
         else if (line.isVertical) {
-            
             isPlayerWithinLineY = (line.y1 < this.y && this.y < line.y2) || (line.y1 < this.y + this.height && this.y + this.height < line.y2) 
             || (this.y < line.y1 && line.y1 < this.y + this.height) || (this.y < line.y2 && line.y2 < this.y + this.height);
             
             isPlayerWithinLineX = this.x < line.x1 && line.x1 < this.x + this.width;
-
         }
 
-       
         return isPlayerWithinLineX && isPlayerWithinLineY;
     }
 
     updateJumpStrength(){
-      
-
         if(this.onPlatform && this.jumpHeld && this.jumpStrength < this.maxJumpStrength){
-            this.jumpStrength = Math.min(this.jumpStrength + 0.1, this.maxJumpStrength)
-            
+            this.jumpStrength = Math.min(this.jumpStrength + 0.1, this.maxJumpStrength) 
         }
-
     }
 
     jump(){
