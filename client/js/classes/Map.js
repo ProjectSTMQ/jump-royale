90cf36ea1667231bccb7a7b405573340247749b7
sterling-tenn <sterling8302@gmail.com>
2023-11-24 02:11:49 -0500

    Update Map.js

--- client/js/classes/Map.js
+++ client/js/classes/Map.js
@@ -14,7 +14,9 @@ class Map{
         this.levelLines.push(new Line(50,900,1150,900))
         this.levelLines.push(new Line(350,500,1150,500))
         this.levelLines.push(new Line(50, 700, 50, 900))
-        this.levelLines.push(new Line(350, 700, 350, 900))
+        this.levelLines.push(new Line(350, 800, 350, 900))
+        this.levelLines.push(new Line(350, 800, 450, 800))
+        this.levelLines.push(new Line(750, 800, 650, 900)) // 45 deg line for test
         this.levelLines.push(new Line(150, 200, 150, 550))
         this.newLevel = new Level(0, this.levelLines)
         this.levels.push(this.newLevel)
